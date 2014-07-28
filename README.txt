3d1a295e72e932062d13fb57a0fd03f60da07631
Xolani Mfuko <xmfuko@gmail.com>
2014-07-28 00:23:17 -0700

    Refactored js code, finished css. Error in js code

---
+++ README.txt
@@ -0,0 +1,32 @@
+--------------
+Scrabble Score
+--------------
+
+Scrabble Score is a webpage that allows users to input a word into
+the webpage and get an out put of the scrabble score for that 
+particular word.
+
+Author
+------
+Xolani Mfuko
+
+
+Operating Instruction
+---------------------
+Input any word in the input box of the page, click on the "score"
+button and the score for that word will be outputted.
+
+Copying/License
+---------------
+This project is licenced under the GNU General Public Licence.
+For more information on the licence, please vist 
+
+http://www.gnu.org/copyleft/gpl.html
+
+BUGS
+----
+There are no known bugs in the code.
+
+Date
+----
+27 July 2014
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 //Creates a function to test for the scrabble score for any word.
 var scrabble = function(word) {
-//Initialise an object for scrabble letters and related numbers
+//Initialise an object for scrabble letters and related numbers.
 	var letterScore = {
 		"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
 		"D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, 
 		"Y": 4, "K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10
 	};
 
-	var wordLetters = [];
 	var totalScore = 0;
 	//Takes the user inputted word and splits it into an array.
-	wordLetters = word.toUpperCase().split("");
+	var wordLetters = word.toUpperCase().split('');
 	//Compares each letter in the inputted word, returns the letters score and adds up the scores for a word score. 
 	for(var i=0; i<wordLetters.length; i++) {
 		totalScore += letterScore[wordLetters[i]];
