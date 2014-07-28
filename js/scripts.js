f7102685095cf3ab2f5e86c9f802c2caa3d1e8d9
Xolani Mfuko <xmfuko@gmail.com>
2014-07-27 20:05:14 -0700

    Finished working code using arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-//Creates an array of all the letters and an array with index related scores 
-var letters = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T", "D", "G", "B", "C", "M", "P", "F", "H", "V", "W", "Y", "K", "J", "X", "Q", "Z"];
-var letterScore = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "4", "5", "8", "8", "10", "10"];
+//Creates a function to test for the scrabble score for any word.
+var scrabble = function(word) {
+//Initialise an array for scrabble letters, an array for the score of each letter, an empty arr
+	var letters = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T", "D", "G", "B", "C", "M", "P", "F", "H", "V", "W", "Y", "K", "J", "X", "Q", "Z"];
+	var letterScore = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10];
+	var wordLetters = [];
+	var totalScore = 0;
+
+	//Takes the user inputted word and splits it into an array.
+	wordLetters = word.toUpperCase().split("");
+
+	//Compares each letter in the inputted word, returns the letters score and adds up the scores for a word score. 
+	for (var j=0; j<wordLetters.length; j++) {
+		for(var i=0; i<letters.length; i++) {
+			if(wordLetters[j] === letters[i]) {
+				totalScore += letterScore[i];
+			}
+		}
+	}
+	return totalScore;
+}
 
 $(document).ready(function() {
 	$("form#form").submit(function(event) {
 		var wordInput = $("input#word-input").val();
-		var positionScore;
-
-		for(var i=0; i<letters.length; i++) {
-			if(wordInput === letters[i]) {
-				positionScore = letterScore[i];
-				console.log(positionScore);
-			}
-		}
 
 		$("span#word").append(wordInput);
-		$("span#show-score").append(positionScore);
+		$("span#show-score").append(scrabble(wordInput));
 		$("#hidden").show();
 
 		$("input#word-input").val("");
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe("scrabble", function() {
-	it("outputs the score for an inputted letter", function() {
+	it("outputs the score for an inputted capital letter", function() {
 		scrabble("A").should.equal(1);
+	});
+	it("outputs the score for an inputted small letter", function() {
+		scrabble("x").should.equal(8);
+	});
+	it("outputs the score for an inputted word in any case", function() {
+		scrabble("fanTaStiC").should.equal(14);
 	});
 });
