661da55d772509b5e66c26e20e53505cb6b846da
Xolani Mfuko <xmfuko@gmail.com>
2014-07-30 22:16:24 -0700

    Fixed bug in code. Assessment complete

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,9 @@ var scrabble = function(word) {
 		"D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, 
 		"Y": 4, "K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10
 	};
-
 	var totalScore = 0;
 	//Takes the user inputted word and splits it into an array.
-	var wordLetters = word.toUpperCase().split('');
+	var wordLetters = word.toUpperCase().split("");
 	//Compares each letter in the inputted word, returns the letters score and adds up the scores for a word score. 
 	for(var i=0; i<wordLetters.length; i++) {
 		totalScore += letterScore[wordLetters[i]];
@@ -19,10 +18,10 @@ var scrabble = function(word) {
 
 $(document).ready(function() {
 	$("form#form").submit(function(event) {
-		var wordInput = $("input#word-input").val();
+		var wordInput = $("input.word-input").val();
 
-		$("span#word").append(wordInput);
-		$("span#show-score").append(scrabble(wordInput));
+		$("span#word").text(wordInput);
+		$("span#show-score").text(scrabble(wordInput));
 		$("#hidden").show();
 
 		$("input#word-input").val("");
